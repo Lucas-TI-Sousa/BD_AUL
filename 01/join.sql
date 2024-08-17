be32ec2348d4aacc09335146eb48d885b7120199
lucas <lucas.ssantos189@senacsp.edu.br>
2024-08-16 22:01:06 -0300

    alteracao

--- 01/join.sql
+++ 01/join.sql
@@ -67,3 +67,79 @@ where e.id = 3
 
 --CONCAT == concatenação
 --round  == vai arrendodar os valores para duas casas decimais.
+
+/*
+Outras formas de agregação
+ 
+ 
+MIN - Permite ver o valor mínimo em um conjunto de resultado
+MAX - Permite ver o valor máximo em um conjunto de resultado
+ 
+*/
+ 
+-- Quero saber qual é o primeiro cliente de Lisboa na tabela cliente.
+select id, nome from clientes where cidade = "Lisboa" LIMIT 1;
+Select MIN(id), nome from clientes where cidade = "Lisboa";
+select MAX(id), nome from clientes where email like "%@gmail.com";
+
+/*
+AVG - Permite calcular a média de um conjunto de resultados NUMÉRICOS
+SUM - Permite ver o somatório (calculo total) de um conjuto de resultados.
+*/
+
+-- média de preço dos produtpos
+Select AVG(preco_unidade) preco_medio from produto;
+Select round(AVG(preco_unidade),2) preco_medio from produto;
+
+select SUM(preco_unidade) total_preco from produtos;
+
+
+
+
+select
+e.id,            --Atributo da tabela encomenda
+e.data_hora,     --Atributo da tabela encomenda
+p.produto,       --Atributo da tabela produto
+p.preco_unidade, --Atributo da tabela produto
+ep.quantidade
+from encomendas_produtos ep
+LEFT JOIN encomendas e 
+on ep.id_encomenda = e.id
+LEFT JOIN produtos p
+on ep.id_produto = p.id
+where e.id = 50;
+
+
+select
+e.id,           
+e.data_hora,    
+p.produto,      
+p.preco_unidade, 
+ep.quantidade,
+SUM(round(p.preco_unidade * ep.quantidade,2)) as total
+from encomendas_produtos ep
+LEFT JOIN encomendas e 
+on ep.id_encomenda = e.id
+LEFT JOIN produtos p
+on ep.id_produto = p.id
+where e.id = 50;
+
+
+--subuqery
+select id_encomenda, id_cliente, SUM(total) total
+from
+(
+    select
+    ep.id_encomenda,
+    e.id_cliente,
+    ep.quantidade,
+    p.produto,
+    CAST(ep.quantidade *p. preco_unidade As decimal(10,2)) As total 
+    from encomendas_produtos ep
+    LEFT join produtos p on ep.id_produto = p.id
+    LEFT join encomendas e on ep.id_encomenda = e.id
+    where e.id = 50
+)a
+Group by id_encomenda
+
+
