1625ec656170f8d0a0c0d72ecf92e2a491e26db7
lucas <lucas.ssantos189@senacsp.edu.br>
2024-08-28 20:46:05 -0300

    loja_ok

---
+++ loja_OK/conexao.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$database = "loja_OK";
+$username = "root";
+$password = "";
+
+//criar a conexao
+$conexao= mysqli_connect($servername, $username, $password, $database);
+
+?>
