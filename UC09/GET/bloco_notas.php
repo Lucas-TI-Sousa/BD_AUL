a24ad04d62164b3f28b4f9ab6169823bf89ce06a
lucas <lucas.ssantos189@senacsp.edu.br>
2024-09-11 22:18:57 -0300

    get post

---
+++ UC09/GET/bloco_notas.php
@@ -0,0 +1,16 @@
+<?
+
+/* 
+Este é um exemplo muito simples do que ser uma API com vários endpoints, mas em qualquer 
+mecanismo de autenticação e sem controle de erros.
+Vamos apenas seguir os seguintes príncios:
+
+1. Qualquer cliente pode ter acesso aos endpoins atráves da URL;
+2. Cada endponit deve responder no formato JSON;
+
+http://localhost/04/api/get_status
+http://localhost/04/api/get_all_animals
+http://localhost/04/api/get_animals
+http://localhost/04/api/get_animals/?max-5
+
+*/
---
+++ UC09/GET/get_animais/index.php
@@ -0,0 +1,21 @@
+<?php
+
+//SET o canteúdo da API
+header("Content-Type:application/json;charset=utf=8");
+
+$data = require_once('../_data/data.php');
+
+if(!isset($_GET['max']) || $_GET['max'] < 1 || $GET['max'] > count($data)) {
+    $response['status'] = 'error';
+    $response['message'] = 'Esta requisiçõa não está definida.';
+    $response['time_response'] = time();
+    echo json_encode($response, JSON_UNESCAPED_UNICODE);
+    exit();
+} 
+
+$max = $_GET['max'];
+
+$response['status'] = 'sucesso!';
+$response['animais'] = array_slice($data, 0, $max);
+$response['time_response'] = time();
+echo json_encode($response, JSON_UNESCAPED_UNICODE);
