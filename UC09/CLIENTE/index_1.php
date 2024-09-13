4edbdebc9d6ca1fbfdd940825a2def4a98ac48ff
lucas <lucas.ssantos189@senacsp.edu.br>
2024-09-12 22:07:13 -0300

    metodos

---
+++ UC09/CLIENTE/index_1.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+Chamar um endpoint da API através do cURL
+(cliente URL)
+Com o protocolo HTTP e o método (verb) GET 
+*/
+echo '<pre>'
+
+$curl = curl_init();
+curl_setopt_array($curl, array(
+CURLOPT_URL => 'http://localhost/BD_AUL/UC09/test_api_1/get_all_clients/',
+CURLOPT_RETORNTRANSFER => true,
+CURLOPT_ENDCODING => '',
+CURLOPT_MAXREDIRS => 0,
+CURLOPT_TIMEOUT => 0,
+CURLOPT_FOLLOWLOCATION => true,
+// CURLOPT_HTTP_VERSION => CURLOPT_HTTP_VERSION_1_1,
+CURLOPT_CUSTOMREQUEST => 'GET',
+));
+$response = curl_exec($curl);
+curl_close($curl);
+echo $response;
+
+?>
+
+
+
+
+
+
